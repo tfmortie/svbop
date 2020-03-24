51621f445c2d1fe8d8597e0bfd2eaca39e229210
tfmortie <thomas.mortier92@gmail.com>
2020-03-24 15:23:02 +0100

    templated vectoarr

--- src/main/cpp/data.h
+++ src/main/cpp/data.h
@@ -18,6 +18,23 @@ void processData(const std::string &file, problem &p);
 std::vector<std::vector<unsigned long>> processHierarchy(const std::string &file);
 std::vector<std::vector<unsigned long>> strToHierarchy(std::string str);
 std::vector<unsigned long> arrToVec(const std::string &s);
-std::string vecToArr(const std::vector<unsigned long> &v);
+
+/* convert vector of arbitrary type to string */
+template <typename T> 
+std::string vecToArr(const std::vector<T> &v)
+{
+    std::string ret_arr;
+    ret_arr += '[';
+    // process all except last element
+    for (unsigned int i=0; i<v.size()-1; ++i)
+    {
+        ret_arr += std::to_string(v[i]);
+        ret_arr += ",";
+    }
+    // and now last element
+    ret_arr += std::to_string(v[v.size()-1]);
+    ret_arr += "]";
+    return ret_arr;
+}
 
 #endif
